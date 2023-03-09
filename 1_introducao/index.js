7df11436b035e574d84c6136eb8e9a8a74fdbafa
Rosemeire <rosemeireribeirobraz54@gmail.com>
2023-03-09 17:08:32 -0300

    Introdução do typescript

---
+++ 1_introducao/index.js
@@ -0,0 +1,17 @@
+var firstName = "Rose";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
+// greeting(anotherName)
+// greeting(x)
+// Desafia 1 
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(4, 4));
+var z = 5;
+var y = 8;
+console.log(soma(z, y));
