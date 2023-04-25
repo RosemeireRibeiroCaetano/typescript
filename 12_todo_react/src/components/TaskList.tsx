3f5f9d91e1aaaa0d6236a1782b3ca5a7d9011099
Rosemeire <rosemeireribeirobraz54@gmail.com>
2023-04-25 19:51:52 -0300

    Html dos itens da lista

--- 12_todo_react/src/components/TaskList.tsx
+++ 12_todo_react/src/components/TaskList.tsx
@@ -1,31 +1,37 @@
-import * as React from 'react'
+import Reactfr from "react";
 
-// Interfaces
-import { ITask } from '../interfaces/Task';
+// Interfaces 
+import { ITask } from "../interfaces/Task";
 
 // Css
 import style from './TaiskList.module.css';
 
-interface Props {
- taskList: ITask[];
+interface Props{
+  taskList: ITask[];
 }
-
-const TaskList = ({taskList} : Props) =>{
-    return (
-      <>
-      {
-        taskList.length > 0 
-        ? 
-        (
-          taskList.map((t, i) => (<div key={i}>{t.title}</div>))
-        )
-        :
-        (<p>Lista vazia</p>)
-      }
-      </>
-      
-    )
-     
+// condiçao ? true : false
+const TaskList = ({taskList} : Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ): (
+      <p>Não há tarefas cadastradas!</p>
+      )} 
+    </>
+  )
+ 
 }
 
 export default TaskList;
