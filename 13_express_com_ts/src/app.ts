4d105a2276d72fe935ff55580781b0d3b2dded0a
Rosemeire <rosemeireribeirobraz54@gmail.com>
2023-05-11 19:54:42 -0300

    Rota para qualquer verbo

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -22,6 +22,21 @@ app.post("/api/product", (req, res) => {
   return res.send("Produto adicionando!");
 });
 
+// 4 - rota para todos verbos
+app.all("/api/product/check", (req, res) => {
+
+    // req.method = VERBO HTTP
+
+    if(req.method === "POST") {
+        return res.send("Inseriu algum registro!");
+    } else if (req.method === "GET") {
+        return res.send("Leu algum registro!");
+    } else {
+        return res.send("Não podemos realizar esta operação!");
+    }
+
+});
+
 app.listen(3000, () => {
   console.log("Aplicação de Ts + Express funcionando!");
 });
