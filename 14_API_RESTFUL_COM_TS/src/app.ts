dfcaa6d18422671de8dca25099af9a233a67ead7
Rosemeire <rosemeireribeirobraz54@gmail.com>
2023-05-24 14:28:02 -0300

    Rota de teste da API

--- 14_API_RESTFUL_COM_TS/src/app.ts
+++ 14_API_RESTFUL_COM_TS/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import config from "config"
+import router from "./router";
 
 const app = express();
 
 // JSON middleware
 app.use(express.json());
+
+//router
+app.use("/api", router);
 
 // app port
 const port = config.get<number>("port");
